91d2d26af9cfd8646ccdff0b082baa4001a80da2
HtetKo510217 <htetko510217@gmail.com>
2023-03-27 21:20:53 +0630

    Map

---
+++ Built-in-data-structures/map.js
@@ -0,0 +1,12 @@
+// Map is a built-in object that allows you to store key-value pairs
+
+const map = new Map([['a',1],['b',2]]);
+map.set('c',3);
+map.delete('c');
+console.log(map.has('a'));
+console.log(map.size);
+map.clear();
+
+for (const [key,value] of map) {
+    console.log(`${key} : ${value}`);
+}
